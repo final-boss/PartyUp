ad9e76bdee3530c6872a08857ad730dc8474e7eb
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-23 19:44:51 -0500

    Added envy

--- mix.exs
+++ mix.exs
@@ -30,14 +30,15 @@ defmodule PartyUp.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.4"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
+    [{:phoenix,             "~> 1.2.4"},
+     {:phoenix_pubsub,      "~> 1.0"},
+     {:phoenix_ecto,        "~> 3.0"},
+     {:postgrex,            ">= 0.0.0"},
+     {:phoenix_html,        "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:gettext,             "~> 0.11"},
+     {:cowboy,              "~> 1.0"},
+     {:envy,                "~> 1.1.1"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
