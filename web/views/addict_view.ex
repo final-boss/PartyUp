233d034197d9fcc03a07bbdf7d82c3ed686fe9f5
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2017-05-31 01:53:18 -0500

    Uncomment addict view imports

--- web/views/addict_view.ex
+++ web/views/addict_view.ex
@@ -1,7 +1,6 @@
 defmodule Addict.AddictView do
   use Phoenix.HTML
   use Phoenix.View, root: "web/templates/"
-  # NOTE: Silences unused warning, import as needed
-  # import Phoenix.Controller, only: [view_module: 1]
-  # import PartyUp.Router.Helpers
+  import Phoenix.Controller, only: [view_module: 1]
+  import PartyUp.Router.Helpers
 end
